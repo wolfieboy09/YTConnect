aeedc8a89290c209c50fd81bd02cb3ac7cf3f45e
Knightbot63-Github <125277461+Knightbot63-Github@users.noreply.github.com>
2023-02-21 10:12:31 -0500

    Removed some things

--- user.py
+++ user.py
@@ -1,11 +1,7 @@
-#ooooKkkJKDJK
-#zinusiqy@finews.biz
-
-#Get a User
-
 import requests
 import json
 
+#Required
 key = "AIzaSyApx8brEEcP1qq_b4hAd74yZNO9sx12xgg"
 
 bold = "\u001b[1m"
@@ -19,24 +15,28 @@ Cyan = "\u001b[36m"
 clear = "\u001b[0m"
 class user():
      def subscriber_count(self):
+          #Although, YouTube API isn't accurate and is rounded. Please be aware.
           print("requesting")
           url = f"https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername={self}&key={key}"
           r = requests.get(url)
           subs = r.json()['items'][0]['statistics']['subscriberCount']
           return subs
      def view_count(user):
+          #Work in Progress
           r = requests.get(f"https://youtube.com/{user}/about").text
           try:
                r.json()['Total Views']
           except:
                print("Failed to Parse ", r)               
      def about(user):
+          #Work in Progress
           r = requests.get(f"https://youtube.com/{user}/about").text
           if r.status_code == 200:
                print("Got")
           else:
                print("Error!")
      def location(user):
+          #Work in Progress
           pass
 '''
 To prevent spam and bot attacks, Comment and Video posting and replying is not a thing with YouTubeConnect and Never will be.
