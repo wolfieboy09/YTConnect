42898346d95b6a7bf7b0dae59410811f29b12e8d
Knightbot63-Github <125277461+Knightbot63-Github@users.noreply.github.com>
2023-03-17 11:51:33 -0400

    Added Session and Exceptions

---
+++ Exceptions.py
@@ -0,0 +1,12 @@
+from . import user
+from . import Session
+
+# <--- Handling Exceptions --->
+class ConnectionAbortedError:
+    pass
+
+class ConnectionError:
+    pass
+
+class ConnectionRefusedError:
+    pass
