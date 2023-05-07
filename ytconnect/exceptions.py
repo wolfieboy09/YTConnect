0c8e4723c5510b6e91b5fab3905c26643245980c
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2023-05-07 19:39:05 -0400

    Fixed Expectation classes

--- ytconnect/exceptions.py
+++ ytconnect/exceptions.py
@@ -2,11 +2,17 @@ from .user import user
 from .session import Session
 
 # <--- Handling Exceptions --->
-class ConnectionAbortedError:
+class ConnectionAbortedError(Expectation):
     pass
 
-class ConnectionError:
+class ConnectionError(Expectation):
     pass
 
-class ConnectionRefusedError:
+class ConnectionRefusedError(Expectation):
+    pass
+
+class ResponceCodeNot200(Expectation):
+    pass
+
+class APIKeyNotGiven(Expectation):
     pass
