0e954b01cb1a71fb17efa92b743130dff3cce502
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2023-05-12 13:30:06 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='requests',
+      version='2.30.0',
+      description='Python Distribution Utilities',
+      author='Greg Ward',
+      author_email='gward@python.net',
+      url='https:/pypi.com/project/requests',
+      packages=['distutils', 'distutils.command'],
+     )
